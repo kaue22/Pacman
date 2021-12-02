50bf8782b3b46dc86ac406747a7ba6e5da81fadb
kaue <kauegouveia@gmail.com>
2021-12-02 08:43:28 -0300

    Criação de circulo e tela
    Criando o primeiro circulo e a tela do jogo

--- pacman.py
+++ pacman.py
@@ -1,3 +1,21 @@
 import pygame
 
+AMARELO = (255,255,0)
 pygame.init()
+
+tela = pygame.display.set_mode((640, 480), 0)
+
+while True:
+
+    #Calcula as regras
+
+    # Pinta tela
+    pygame.draw.circle(tela, AMARELO, (320, 240), 50, 5)
+    pygame.display.update()
+
+    #Eventos do user
+
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            exit()
+
